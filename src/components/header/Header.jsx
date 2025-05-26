b87a9ff428b62f0eed8ce86c16fa9af547da4c8d
Somayeh Poolayi <somayeh.ply@gmail.com>
2025-05-26 12:51:34 -0700

    home page-v1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,20 @@
-import styled from "./header.module.css"
-function Header(props){
-  return(
-<div className={styled.header}>
-<ul>
-  <li>خروج</li>
-  <li>درباره من</li>
-  <li>ساخت مقاله</li>
-  <li>جستجوی مقاله</li>
-  <li>لیست مقالات</li>
-  <h3>{props.title}</h3>
-</ul>
-</div>
-  )
+import styled from "./header.module.css";
+function Header(props) {
+  return (
+    <div className={styled.headerWrapper}>
+      <div className="container">
+        <div className={styled.header}>
+          <ul>
+            <li>خروج</li>
+            <li>درباره من</li>
+            <li>ساخت مقاله</li>
+            <li>جستجوی مقاله</li>
+            <li>لیست مقالات</li>
+            <h3>{props.title}</h3>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Header;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import styled from "./home.module.css";
+function Home() {
+  return (
+    <div className={styled.homeWrapper}>
+      <div className="container">
+        <h1>Welcome my friend ...</h1>
+      </div>
+    </div>
+  );
+}
+export default Home;
